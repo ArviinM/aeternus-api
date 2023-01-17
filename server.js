081d9cf0d0687628f4edbc56db96e829cbec371a
Arvin M <rhenmedina20@gmail.com>
2023-01-17 20:42:56 +0800

    renamed origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 var corsOptions = {
-  origin: "https://aeternus-frontend.onrender.com/",
+  origin: "https://aeternus-frontend.onrender.com",
   credentials: true,
 };
 
