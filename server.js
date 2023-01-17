e090fbded17d309818afe50993e6a2f30b83dfb0
Arvin M <rhenmedina20@gmail.com>
2023-01-17 20:36:23 +0800

    new updates hosting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://aeternus-frontend.onrender.com/",
   credentials: true,
 };
 
